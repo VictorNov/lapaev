7f2ccb04ee7949a0242a8feb8b44961e7c9fbc05
Victor Novokshenov <between.photo@gmail.com>
2024-09-11 00:56:05 +0200

    Fix build

---
+++ pages/blog/tinder.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="index-page">
+    Blog Page
+  </div>
+</template>
+
+<style scoped lang="scss">
+.index-page {
+  padding-bottom: var(--spacing-6xl);
+}
+</style>
